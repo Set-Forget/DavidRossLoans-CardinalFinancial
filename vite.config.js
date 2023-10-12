f8da5a4cda4b5fafc98f3b91e2e8329ec70c8b69
Fabricio Montivero <fabricio@setandforget.io>
2023-10-12 15:22:48 -0300

    se cambio basepath

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DavidRossLoans-Comps-Home-Value-Sites/"
+  base: "/DavidRossLoans-CardinalFinancial/"
 })
