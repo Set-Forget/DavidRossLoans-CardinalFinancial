f807ba6a2aac4174d8acafb4156b2b2255df4666
Fabricio Montivero <fabricio@setandforget.io>
2023-10-17 17:50:48 -0300

    se agrego conexion con pipedrive

--- src/page/searchPage.jsx
+++ src/page/searchPage.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useMutation } from 'react-query';
 import Spinner from "./../components/Spinner"
 import WebScrappingResult from "./../components/TableResults"
 import SearchBar from "./../components/searchBar/MapboxSearchBar"
 
 export default function SearchPage() {
-    const [hasSearched, setHasSearched] = useState(false);
-    const [average, setAverage] = useState(0);
+    const [hasSearched, setHasSearched] = useState(true); // esta en true para testear, sino tiene que ser false
+    const [average, setAverage] = useState(100); // tiene valor para testear, sino tiene que ser 0
     const [searchItem, setSearchItem] = useState("")
     const companies = [
         {title: "Homebot", img:"https://imgs.search.brave.com/6Laiiyi2pSBnp5knhBT0xjKcyVnuSgYnNc-A92zgY58/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/c2NsbW9ydGdhZ2Uu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIzLzAyL2hvbWVi/b3QtbG9nby0yOTN4/MzAwLnBuZw"},
@@ -15,7 +15,18 @@ export default function SearchPage() {
         {title: "Realtor", img:"https://imgs.search.brave.com/0aJD-DQCf4FViZMXm0kgCk05J17qx48pVme8QX6kkEw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hY3Jl/YWxlc3RhdGUub3Jn/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDEz/LzEwL1JlYWx0b3It/TG9nby0yNjN4MzAw/LmpwZw"}
     ]
 
-    const [webInfo, setWebInfo] = useState([])
+    const [webInfo, setWebInfo] = useState([{ // tiene datos para testear, sino tiene que ser []
+      estimatedValue:1,
+      websiteName: "Realtor"
+    },
+    {
+      estimatedValue:2,
+      websiteName: "Zillow"
+    },
+    {
+      estimatedValue:3,
+      websiteName: "Corelogic"
+    }])
     
     const searchRealStateValues = async (address) => {
         const url = "https://script.google.com/macros/s/AKfycbwzIyZEUg78F3IbJvYJHNVPgqkwLIBnb4Lz0Y_cErcYxBKipyj-QoM5WKaj5oO6gFgnog/exec" + "?address=" + address
@@ -55,9 +66,64 @@ export default function SearchPage() {
               <hr />
               </>):
               hasSearched ?
-              webInfo && <WebScrappingResult searchItem={searchItem} companies={companies} webInfo={webInfo} avg={average}/>:
+              webInfo && (
+              <>
+                <WebScrappingResult searchItem={searchItem} companies={companies} webInfo={webInfo} avg={average}/>
+                <ButtonAddToPipedrive searchItem={searchItem} companies={companies} webInfo={webInfo} avg={average}/>
+              </>
+              ):
               <p>Make a search to find the values</p>
             }
         </>
       )
+}
+
+function ButtonAddToPipedrive({searchItem, companies, webInfo, avg }) {
+    const [deals, setDeals] = useState([])
+    const [selectedDeal, setSelectedDeal] = useState(null)
+    const apiUrl = "https://api.pipedrive.com/v1"
+    const apiKey = "6a7d6756d454c725103f483026d2773693b1d5fc"
+
+    const updateDeal = function () {
+      if (selectedDeal) {
+        const apiEndpoint = `/deals/${selectedDeal}?api_token=${apiKey}`;
+        fetch(apiUrl + apiEndpoint, {
+          method: "PUT",
+          body: JSON.stringify({
+            "value": avg,
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        })
+      }
+    }
+
+    const handleSelectChange = (event) => {
+      setSelectedDeal(event.target.value);
+    }
+
+    useEffect(()=>{
+      const apiEndpoint = "/deals?status=all_not_deleted&start=0&limit=500&api_token="
+      fetch(apiUrl + apiEndpoint + apiKey)
+      .then(response => response.json())
+      .then( res => setDeals(res.data))
+    }, [])
+
+    return(
+      <section className="flex gap-6">
+        <div className="flex gap-4 items-center">
+          <select 
+           id="deals"
+           onChange={handleSelectChange}
+           value={selectedDeal || ''}
+           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option defaultValue={0} hidden>Select a deal</option>
+            { deals.map( d => <option value={d.id} key={d.id}>{d.title}</option>) }
+          </select>
+          to
+        </div>
+        <button type="button" onClick={updateDeal} className="text-white bg-[#00B1A4] hover:bg-[#033652] focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 focus:outline-none dark:focus:ring-blue-800">add to pipedrive</button>
+      </section>
+    )
 }
