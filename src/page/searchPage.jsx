4047525f021c3184393cac2c60ccb72b61ff52e9
Fabricio Montivero <fabricio@setandforget.io>
2023-10-22 11:59:02 -0300

    se comento 'homebot'

--- src/page/searchPage.jsx
+++ src/page/searchPage.jsx
@@ -10,7 +10,7 @@ export default function SearchPage() {
     const [average, setAverage] = useState(0);
     const [searchItem, setSearchItem] = useState("")
     const companies = [
-        {title: "Homebot", img:"https://imgs.search.brave.com/6Laiiyi2pSBnp5knhBT0xjKcyVnuSgYnNc-A92zgY58/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/c2NsbW9ydGdhZ2Uu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIzLzAyL2hvbWVi/b3QtbG9nby0yOTN4/MzAwLnBuZw"},
+        // {title: "Homebot", img:"https://imgs.search.brave.com/6Laiiyi2pSBnp5knhBT0xjKcyVnuSgYnNc-A92zgY58/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/c2NsbW9ydGdhZ2Uu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIzLzAyL2hvbWVi/b3QtbG9nby0yOTN4/MzAwLnBuZw"},
         {title: "Zillow", img:"https://s.zillowstatic.com/pfs/static/z-logo-default.svg"},
         {title: "Corelogic", img:"https://imgs.search.brave.com/PNU0Uym5dsCc1rqpNhFF5itvNhvSWJlxiUUpNJmS0OY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy80/LzRkL0NvcmVMb2dp/Y19sb2dvLnN2Zw.svg"},
         {title: "Realtor", img:"https://imgs.search.brave.com/0aJD-DQCf4FViZMXm0kgCk05J17qx48pVme8QX6kkEw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hY3Jl/YWxlc3RhdGUub3Jn/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDEz/LzEwL1JlYWx0b3It/TG9nby0yNjN4MzAw/LmpwZw"}
@@ -31,7 +31,8 @@ export default function SearchPage() {
         let value = Number(i.estimatedValue);
         return isNaN(value) ? 0 : value;
         }).filter(value => value !== 0)
-        const avg = infoValues.reduce((acc, num) => acc + num, 0) / infoValues.length
+
+        const avg = infoValues.length != 0 ? infoValues.reduce((acc, num) => acc + num, 0) / infoValues.length : 0
         setAverage(avg)
         setWebInfo(info.results)
         setHasSearched(true);
