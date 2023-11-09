9a517c00961228d447207ae89a5badc9206109fa
Fabricio Montivero <fabricio@setandforget.io>
2023-11-09 17:55:40 -0300

    se agrego componente para manejar estado de resultados de companias

--- src/components/TableResults.jsx
+++ src/components/TableResults.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function WebScrappingResult({searchItem, companies, webInfo, avg }) {
+export default function WebScrappingResult({searchItem, companies, avg }) {
     return(
         <>
         <div className="flex gap-2">
@@ -10,9 +10,7 @@ export default function WebScrappingResult({searchItem, companies, webInfo, avg 
         <div className="flex flex-wrap place-content-center gap-8 w-full">
         {companies.map(
             (items, i) => {
-            const itemInfo = webInfo.find(web => web.websiteName == items.title)
-            const value = itemInfo ? itemInfo?.estimatedValue ? itemInfo?.estimatedValue: "0" : "0"
-            return <CardForValues title={items.title} img={items.img} value={ value } link={items.link} key={i}/>}
+            return <CardForValues title={items.title} img={items.img} value={ items.estimatedValue } link={items.link} key={i}/>}
         )}
         </div>
         <p>Average: <span>{avg.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span></p>
@@ -23,12 +21,14 @@ export default function WebScrappingResult({searchItem, companies, webInfo, avg 
 function CardForValues({title, img, value, link}) {
     const formatted = parseFloat(value).toLocaleString('en-US', { style: 'currency', currency: 'USD' })
     return(
-        <a href={link} target="_blank">
-            <div className="bg-gray-100 dark:bg-slate-700 border-solid border-2 border-[#00B1A4] p-4 rounded min-w-min w-48 h-48 flex flex-col relative items-end justify-between">
-            <img src={img} alt={title} className="self-start justify-self-start h-12"/>
-            <p className="font-semibold ">{formatted}</p>
-            </div>
-        </a>
+        <div className="relative w-48 h-48">
+            <a href={link} target="_blank" >
+                <div className="bg-gray-100 dark:bg-slate-700 border-solid border-2 border-[#00B1A4] p-4 rounded min-w-min w-48 h-48 flex flex-col relative items-end justify-between">
+                <img src={img} alt={title} className="self-start justify-self-start h-12"/>
+                </div>
+            </a>
+            <p className="font-semibold absolute bottom-2 right-2" contentEditable={true} >{formatted}</p>
+        </div>
     )
 }
 
--- src/page/searchPage.jsx
+++ src/page/searchPage.jsx
@@ -6,24 +6,32 @@ import SearchBar from "./../components/searchBar/MapboxSearchBar"
 import ButtonAddToPipedrive from "../components/addNotes/btnAddToPipedrive";
 
 export default function SearchPage() {
-    const [hasSearched, setHasSearched] = useState(false);
+    const [hasSearched, setHasSearched] = useState(true);
     const [average, setAverage] = useState(0);
     const [searchItem, setSearchItem] = useState("")
-    const companies = [
+    const [companies, setCompanies] = useState([
         // {title: "Homebot", img:"https://imgs.search.brave.com/6Laiiyi2pSBnp5knhBT0xjKcyVnuSgYnNc-A92zgY58/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/c2NsbW9ydGdhZ2Uu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIzLzAyL2hvbWVi/b3QtbG9nby0yOTN4/MzAwLnBuZw"},
         {title: "Zillow", 
          img:"https://s.zillowstatic.com/pfs/static/z-logo-default.svg",
-         link:`https://www.zillow.com/homes/${encodeURIComponent(searchItem.split(" ").join("-"))}`},
+         link:`https://www.zillow.com/homes/${encodeURIComponent(searchItem.split(" ").join("-"))}`,
+         estimatedValue:0
+        },
         {title: "Corelogic",
-         img:"https://imgs.search.brave.com/PNU0Uym5dsCc1rqpNhFF5itvNhvSWJlxiUUpNJmS0OY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy80/LzRkL0NvcmVMb2dp/Y19sb2dvLnN2Zw.svg",
-         link:"https://www.chase.com/personal/mortgage/calculators-resources/home-value-estimator"},
+        img:"https://imgs.search.brave.com/PNU0Uym5dsCc1rqpNhFF5itvNhvSWJlxiUUpNJmS0OY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy80/LzRkL0NvcmVMb2dp/Y19sb2dvLnN2Zw.svg",
+        link:"https://www.chase.com/personal/mortgage/calculators-resources/home-value-estimator",
+        estimatedValue:0
+        },
         {title: "Red Fin",
          img:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Redfin_logo.png/270px-Redfin_logo.png",
-         link:"https://www.redfin.com/"},
+         link:"https://www.redfin.com/",
+         estimatedValue:0
+        },
         {title: "Realtor",
          img:"https://imgs.search.brave.com/0aJD-DQCf4FViZMXm0kgCk05J17qx48pVme8QX6kkEw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hY3Jl/YWxlc3RhdGUub3Jn/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDEz/LzEwL1JlYWx0b3It/TG9nby0yNjN4MzAw/LmpwZw",
-         link:"https://www.realtor.com/"}
-    ]
+         link:"https://www.realtor.com/",
+         estimatedValue:0
+        }
+    ])
 
     const [webInfo, setWebInfo] = useState([])
     
@@ -41,6 +49,16 @@ export default function SearchPage() {
         return isNaN(value) ? 0 : value;
         }).filter(value => value !== 0)
 
+        const nc = companies.map( item => {
+          const itemInfo = info.results.find(web => web.websiteName == item.title)
+          console.log(itemInfo);
+          const value = itemInfo ? itemInfo?.estimatedValue ? itemInfo?.estimatedValue: "0" : "0"
+          item.estimatedValue = value
+          return item
+        })
+
+        console.log(nc);
+        setCompanies(nc)
         const avg = infoValues.length != 0 ? infoValues.reduce((acc, num) => acc + num, 0) / infoValues.length : 0
         setAverage(avg)
         setWebInfo(info.results)
