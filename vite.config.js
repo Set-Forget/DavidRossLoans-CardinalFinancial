da58346d9ae5348f27c7e16172dca81e23896114
Fabricio Montivero <fabricio@setandforget.io>
2023-11-27 12:44:58 -0300

    se pasa la variable como string

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/DavidRossLoans-CardinalFinancial/",
-
-// })
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
