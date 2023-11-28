345a801fc205a1c70174b122e3655775fb2427e1
Fabricio Montivero <fabricio@setandforget.io>
2023-11-28 16:47:55 -0300

    se modifico configuracion de .env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    define: {
-      'process.env': env,
-    },
-    plugins: [react()],
-    base: "/DavidRossLoans-CardinalFinancial/"
-  }
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '')
+//   return {
+//     define: {
+//       'process.env': env,
+//     },
+//     plugins: [react()],
+//     base: "/DavidRossLoans-CardinalFinancial/"
+//   }
+// })
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/DavidRossLoans-CardinalFinancial/"
 })
