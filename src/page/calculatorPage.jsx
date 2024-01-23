01e3f01f5f51e493c926a4370636cfc5d7d2bff0
Juan Canneva <juancanneva@setandforget.io>
2024-01-23 10:22:24 -0300

    added go back button

--- src/page/calculatorPage.jsx
+++ src/page/calculatorPage.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import Table from "../components/calculator/Table";
 import TableResult from "../components/calculator/TableResult";
 import { AddIcon } from "../components/calculator/Icons";
+import { ChevronLeftIcon } from "@heroicons/react/20/solid";
 import { CalculatorContext } from "../context/CalculatorContext";
 import { LoadingContext } from "../context/LoadingContext";
 import { MAX_SCENARIOS } from "../utils/utils";
@@ -19,33 +20,33 @@ export default function CalculatorPage() {
   const apiKey = import.meta.env.VITE_PIPEDRIVE_API_KEY;
 
   const getDealById = useCallback(() => {
-    setLoading(true)
+    setLoading(true);
     const apiEndpoint = `/deals/${id}?api_token=`;
     try {
-    fetch(apiUrl + apiEndpoint + apiKey)
-      .then((response) => response.json())
-      .then((res) => {
-        if (!res.success) {
-          navigate(`${BASE_URL}calculator`);
-          return;
-        }
-        dispatch({
-          type: "SET_DEAL",
-          payload: {
-            purchasePrice: res.data[KEY_PURCHASE_PRICE],
-            loanAmount: res.data["value"],
-            downPaymentPercentage: (
-              (1 - res.data["value"] / res.data[KEY_PURCHASE_PRICE]) *
-              100
-            ).toFixed(2),
-            loanTerm: String(res.data[KEY_LOAN_TERM] / 12),
-            HOAPayment: res.data[KEY_HOA_PAYMENT],
-            propertyTaxes: res.data[KEY_PROPERTY_TAXES],
-            downPaymentAmount: res.data[KEY_DOWN_PAYMENT_AMOUNT],
-            mortgageInsurance: res.data[KEY_MORTGAGE_INSURANCE],
-          },
+      fetch(apiUrl + apiEndpoint + apiKey)
+        .then((response) => response.json())
+        .then((res) => {
+          if (!res.success) {
+            navigate(`${BASE_URL}calculator`);
+            return;
+          }
+          dispatch({
+            type: "SET_DEAL",
+            payload: {
+              purchasePrice: res.data[KEY_PURCHASE_PRICE],
+              loanAmount: res.data["value"],
+              downPaymentPercentage: (
+                (1 - res.data["value"] / res.data[KEY_PURCHASE_PRICE]) *
+                100
+              ).toFixed(2),
+              loanTerm: String(res.data[KEY_LOAN_TERM] / 12),
+              HOAPayment: res.data[KEY_HOA_PAYMENT],
+              propertyTaxes: res.data[KEY_PROPERTY_TAXES],
+              downPaymentAmount: res.data[KEY_DOWN_PAYMENT_AMOUNT],
+              mortgageInsurance: res.data[KEY_MORTGAGE_INSURANCE],
+            },
+          });
         });
-      });
     } catch (error) {
       console.error(error);
     } finally {
@@ -60,8 +61,7 @@ export default function CalculatorPage() {
 
   const showAddScenario = scenarios.length < MAX_SCENARIOS;
 
-
-  if (loading) return <Spinner isLayout/>
+  if (loading) return <Spinner isLayout />;
 
   return (
     <>
@@ -72,6 +72,13 @@ export default function CalculatorPage() {
       <section className="rounded-xl">
         <div className="max-w-6xl shadow-sm my-2 rounded-xl">
           <div className="w-full flex justify-between my-6">
+            <button onClick={handleGoBack} className="flex items-center">
+              <ChevronLeftIcon
+                className="h-5 w-5 text-white"
+                aria-hidden="true"
+              />
+              Go back
+            </button>
             <button
               className="flex items-center text-md font-medium whitespace-nowrap mb-1 border border-white rounded-full px-3 py-2 text-white"
               onClick={handleAddScenario}
@@ -107,6 +114,18 @@ export default function CalculatorPage() {
       </section>
     </>
   );
+
+  function handleGoBack() {
+    navigate(-1);
+    dispatch({
+      type: "SET_DEAL",
+      payload: null,
+    });
+    dispatch({
+      type: "SET_SELECTED_DEAL",
+      payload: null,
+    });
+  }
 
   function handleReset() {
     dispatch({ type: "RESET" });
